5b3a782cfae07376e252a2b8ca3d780fccc2e367
anshRastogi02 <rastogiansh2@gmail.com>
2025-02-14 22:57:39 +0530

    [Added Feat] Advanced Multi Sink Venting Simulation

--- Scripts/event_button.gd
+++ Scripts/event_button.gd
@@ -1,6 +1,5 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,4 +11,6 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
+	print("Start Button Pressed")
+	$SfxClickStart.play()
 	ConveyerController.can_send = true
