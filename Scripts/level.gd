669c9f71dbb2596e3bd42fe60bcd86fee2c295f9
Ankita Jana <ankitajana60@gmail.com>
2025-05-16 13:21:45 +0530

    Create level.gd

---
+++ Scripts/level.gd
@@ -0,0 +1,13 @@
+extends Node
+var sinkBoxMatch
+var dlsRequired
+var dlsUsed
+var totalbox
+var nextLevel
+
+func initialise():
+	sinkBoxMatch=false
+	dlsRequired=false
+	dlsUsed=false
+	totalbox=0
+	nextLevel=false
