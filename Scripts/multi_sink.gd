57d71245b6a215685a523afe45a897361bd544eb
Ankita Jana <ankitajana60@gmail.com>
2025-05-23 01:09:21 +0530

    Update multi_sink.gd

--- Scripts/multi_sink.gd
+++ Scripts/multi_sink.gd
@@ -20,23 +20,6 @@ func start1():
 	print("No of events: ",ConveyerController.events.size())
 	ConveyerController.can_send=true
 
-func start():
-	var blueStart=0
-	var extra=ConveyerController.destination.size()-1
-	print(extra)
-	
-	var blueBox = $BoxB
-	var packed = PackedScene.new()
-	packed.pack(blueBox)
-	for m in extra:
-		var clone = packed.instantiate()
-		add_child(clone)
-		var box=ConveyerController.events.pop_back()
-		print(box)
-		ConveyerController.events.insert(blueStart,box)
-	print(ConveyerController.events.size())
-	ConveyerController.can_send=true
-
 
 func _on_button_pressed() -> void:
 	start1()
