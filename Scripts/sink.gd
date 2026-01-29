6cec238bc3712120d935a14a9f89995145c4245e
Tusharjamdade <tusharnjamdade@gmail.com>
2026-01-29 18:47:57 +0000

    bug fix
    Signed-off-by: Tusharjamdade <tusharnjamdade@gmail.com>

--- Scripts/sink.gd
+++ Scripts/sink.gd
@@ -2,15 +2,11 @@ extends Sprite2D
 
 @export var expectedType: String
 
-var _available := true
-
 @export var available: bool = true:
 	set(value):
-		_available = value
+		available = value
 		if indicator_light:
 			indicator_light.visible = value
-	get:
-		return _available
 
 @onready var indicator_light: Control = $IndicatorLight
 
@@ -21,4 +17,4 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if available and indicator_light:
 		var time = Time.get_ticks_msec() / 200.0
-		indicator_light.modulate.a = 0.6 + sin(time) * 0.4
+		indicator_light.modulate.a = 0.6 + (sin(time) * 0.4)
