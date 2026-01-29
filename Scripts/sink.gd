ef7f856ec315cb0a2025623c470f87e7fa826106
Tusharjamdade <tusharnjamdade@gmail.com>
2026-01-29 18:37:36 +0000

    feat: Add sink availability indicator with blinking light
    Signed-off-by: Tusharjamdade <tusharnjamdade@gmail.com>

--- Scripts/sink.gd
+++ Scripts/sink.gd
@@ -2,12 +2,23 @@ extends Sprite2D
 
 @export var expectedType: String
 
+var _available := true
 
-# Called when the node enters the scene tree for the first time.
+@export var available: bool = true:
+	set(value):
+		_available = value
+		if indicator_light:
+			indicator_light.visible = value
+	get:
+		return _available
+
+@onready var indicator_light: Control = $IndicatorLight
+
 func _ready() -> void:
-	pass # Replace with function body.
+	if indicator_light:
+		indicator_light.visible = available
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	if available and indicator_light:
+		var time = Time.get_ticks_msec() / 200.0
+		indicator_light.modulate.a = 0.6 + sin(time) * 0.4
