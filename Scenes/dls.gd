dd4163aca90cfcee9f0730a966286948bcce4279
iwdie <naveenworkstation@gmail.com>
2026-01-31 17:37:35 +0530

    hover label reimplementation

--- Scenes/dls.gd
+++ Scenes/dls.gd
@@ -1,21 +1,21 @@
-extends Sprite2D
-
-@export var expectedType: String
+extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(delta):
 	pass
 
 
-func _on_area_2d_mouse_entered():
+
+func _on_mouse_entered():
 	$hoverlabel.visible = true # Replace with function body.
 
 
-func _on_area_2d_mouse_exited():
-	$hoverlabel.visible = false# Replace with function body.
+
+func _on_mouse_exited():
+	$hoverlabel.visible = false # Replace with function body.
--- Scripts/event_box.gd
+++ Scripts/event_box.gd
@@ -3,10 +3,38 @@ extends Sprite2D
 @export var boxType: String
 @export var sending = false
 
+
+
 func _ready() -> void:
 	print("event ready")
 	print("event is color", boxType)
 	ConveyerController.events.append(self)
+	var stylebox = $hoverlabel.get_theme_stylebox("normal")
+	if boxType == 'Red':
+		
+		if stylebox is StyleBoxFlat:
+			stylebox = stylebox.duplicate()
+			stylebox.bg_color = Color(1.0, 0.424, 0.475)
+			$hoverlabel.add_theme_stylebox_override("normal", stylebox)
+			$hoverlabel.set_text("[center]EVENT R")
+			
+	elif boxType == "Blue":
+		$hoverlabel.position.x += 300
+		if stylebox is StyleBoxFlat:
+			stylebox = stylebox.duplicate()
+			stylebox.bg_color = Color(0.431, 0.565, 1.0)
+			$hoverlabel.set_text("[center]EVENT B")
+			$hoverlabel.add_theme_stylebox_override("normal", stylebox)
+		
+	elif boxType == "Green":
+		$hoverlabel.position.x += 300
+		$hoverlabel.position.y +=  100
+		if stylebox is StyleBoxFlat:
+			stylebox = stylebox.duplicate()
+			stylebox.bg_color = Color(0.31, 0.553, 0.204)
+			$hoverlabel.add_theme_stylebox_override("normal", stylebox)
+			$hoverlabel.set_text("[center]EVENT G")
+			
 
 func _input_event(viewport, event, shape_idx) -> void:
 	print(event)
@@ -16,3 +44,14 @@ func _input_event(viewport, event, shape_idx) -> void:
 func on_click():
 	print("hi")
 	ConveyerController.selected = self
+
+
+func _on_area_2d_mouse_entered():
+	$hoverlabel.visible = true; 
+
+
+
+
+
+func _on_area_2d_mouse_exited():
+	$hoverlabel.visible = false# Replace with function body.
