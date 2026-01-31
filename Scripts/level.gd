1b0c346354d0176f02fc4bd4cf86d0855039e0ca
Tusharjamdade <tusharnjamdade@gmail.com>
2026-01-31 10:13:32 +0000

    add level progress indicator to show learner progression across levels

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -1,4 +1,5 @@
 extends Node
+
 var sinkBoxMatchNeeded=[false,true,true,false]
 var sinkBoxMatchPresent
 var sinkUsed
@@ -9,14 +10,32 @@ var nextLevel
 var levels=["basicEventFlow","boxClick","multiSink","dlqPattern"]
 var levelind=0
 
+# HUD Handling
+var hud_scene = preload("res://Scenes/LevelHUD.tscn")
+var hud_instance
+
+func _ready():
+	hud_instance = hud_scene.instantiate()
+	add_child(hud_instance)
+	update_hud()
+
 func initialise():
 	sinkBoxMatchPresent=true
 	sinkUsed=false
 	totalbox=0
 	nextLevel=false
 	dlsUsed=false
+	
+	# Make sure HUD is visible again if we restarted
+	if hud_instance:
+		hud_instance.show()
+	update_hud()
 
-func  next_level():
+func update_hud():
+	if hud_instance:
+		hud_instance.update_display(levelind + 1, levels.size())
+
+func next_level():
 	if sinkUsed:
 		if not sinkBoxMatchNeeded[levelind] and not dlsRequired[levelind]:
 			print("if next level entered",sinkBoxMatchNeeded,dlsRequired)
@@ -36,13 +55,19 @@ func  next_level():
 		message_display.show_message("Success")
 		await message_display.show_message_for_duration(2.0)
 		message_display.visible = false
+		
 		levelind+=1
+		update_hud()
+		
 		if levelind!=levels.size():
 			var next_level_path="res://Scenes/"+levels[levelind]+".tscn"
 			get_tree().change_scene_to_file(next_level_path)
 			ConveyerController.initialise()
 		else:
 			print("End of Levels.")
+			# --- HIDE HUD HERE ---
+			if hud_instance:
+				hud_instance.hide()
 			get_tree().change_scene_to_file("res://Scenes/end_of_all_levels.tscn")
 	else:
 		print("Failed. Try Again")
